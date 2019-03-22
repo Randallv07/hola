7a4d421e8bf4f01d8ae60ebc26cdeb4287610c1b
Randall <randallv07@gmail.com>
2019-03-22 14:49:25 -0600

    Estadistica. Desviacion Estandar
    Agregue funcion de desviacion estandar

--- Estadistica.py
+++ Estadistica.py
@@ -8,5 +8,11 @@ N=10
 
 data = [random.randint(0,100) for _ in range (N) ]
 
+
+def dev_std(data):
+    prom = mean(data)
+    return ((sum([(x-prom)**2 for x in data]))**0.5)/len(data)
+
 print("Data: ", data)
-print(mean(data))
+print("mean; ", mean(data))
+print("Desvacion estandar: ", dev_std(data))
